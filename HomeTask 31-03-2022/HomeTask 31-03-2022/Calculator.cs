7c2f57f860f30ad3ffd9bd6e98912138fdb2406c
SevdaKhidirova <70jrjy2@code.edu.az>
2022-03-31 20:34:08 +0400

    Task

---
+++ HomeTask 31-03-2022/HomeTask 31-03-2022/Calculator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTask_31_03_2022
+{
+ /*Static Calculator class-ı olsun. 
+
+İki Int qəbul edib Int qaytaran Sum 
+İki Int qəbul edib Int qaytaran Subtract
+
+İki Int qəbul edib Int qaytaran Multiply
+
+İki Int qəbul edib Int qaytaran Divide
+
+Static Metodları olsun. Adlarına uyğun işi görsünlər.*/
+
+
+    static class Calculator
+    {
+        public static int Sum(int a,int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                Console.WriteLine("You can't divide by 0" );
+                return -1;
+            }
+        }
+        /*Int-lərə bir Power extension-u yazın. Rəqəm qəbul edib hansı intdən çağırılıbsa onu qəbul etdiyi rəqəm qüvvətinə qaldırıb qaytarsın.*/
+        // taskın şərtini belə başa düşdüm ki 4 daxil eləsək 4^4 ,7 daxil eləsək 7^7 ni  və s. hesablasın
+
+        public static int Power(this int a)
+        {
+            int result = 1;
+            if (a > 0)
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    result *= a;
+                }
+                return result;
+            }
+            else 
+            {
+                a *= -1;
+                for (int i = 0; i < a; i++)
+                {
+                    result *= a;
+                }
+                Console.Write("1/");
+                return result;
+            }
+        }
+    }
+}
---
+++ HomeTask 31-03-2022/HomeTask 31-03-2022/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace HomeTask_31_03_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 2;
+            while (input > 0 && input <= 5)
+            {
+                Console.WriteLine("1.Sum\n2.Subtract\n3.Divide\n4.Mulitply\n5.power");
+                input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("enter first input: ");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("enter second input: ");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(Calculator.Sum(a, b));
+                        break;
+
+                    case 2:
+                        Console.WriteLine("enter first input: ");
+                        int x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("enter second input: ");
+                        int y = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(Calculator.Subtract(x, y));
+                        break;
+                    case 3:
+                        Console.WriteLine("enter first input: ");
+                        int z = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("enter second input: ");
+                        int t = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(Calculator.Divide(z, t));
+                        break;
+                    case 4:
+                        Console.WriteLine("enter first input: ");
+                        int c = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("enter second input: ");
+                        int d = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(Calculator.Multiply(c, d));
+                        break;
+                    case 5:
+                        Console.WriteLine("pls enter value");
+                        int value = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(value.Power());
+                        break;
+
+                    default:
+                        Console.WriteLine("Not foud");
+                        break;
+                }
+            }
+        }
+    }
+}
